aaeab5fd177e3d40bbc6d5d2a485c4b39ff804db
alyfa <alyfadel67@gmail.com>
2022-12-20 14:22:26 +0200

    handling FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yallah_chat/core/resources/theme_manager.dart';
 import 'package:yallah_chat/core/utils/constance/constance.dart';
 import 'package:yallah_chat/core/utils/network/local.dart';
 import 'package:yallah_chat/core/utils/observer/observer.dart';
+import 'package:yallah_chat/core/utils/widgets/toast_state.dart';
 import 'package:yallah_chat/modules/home/cubit/cubit.dart';
 import 'package:yallah_chat/modules/home/screens/home_layout.dart';
 import 'package:yallah_chat/modules/login/screens/login.dart';
 
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  showToast(text: 'On Message background App', state: ToastState.success);
+}
 void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  var token = await FirebaseMessaging.instance.getToken();
+  print('Token $token');
+  // when application is opened send message
+  FirebaseMessaging.onMessage.listen((event) {
+    print(event.data.toString());
+    showToast(text: 'On Message', state: ToastState.success);
+  });
+  // when application is opened in foreground send message
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print(event.data.toString());
+    showToast(text: 'On Message opened App', state: ToastState.success);
+  });
+  // when application is closed in background send message
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   await CacheHelper.init();
   Bloc.observer = MyBlocObserver();
   uId = CacheHelper.getData(key: 'uId');
